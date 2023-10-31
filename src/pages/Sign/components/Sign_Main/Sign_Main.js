3929537256e60bac940a3967653c65ef813b49b4
hipes <ansj2916@gmail.com>
2023-10-31 17:46:35 +0900

    Sign_main작업

--- src/pages/Sign/components/Sign_Main/Sign_Main.js
+++ src/pages/Sign/components/Sign_Main/Sign_Main.js
@@ -1,11 +1,26 @@
-import { Container } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import style from "./Sign_Main.module.css"
 const Sign_Main = () => {
     return (
         <Container>
             <div className={style.header}>
                 전자결제 홈
+                <hr />
             </div>
+
+            <div className={style.body}>
+                결제 대기중인 문서
+                <div></div>
+                <div>승인할 문서 11건이 있습니다.</div>
+                <Row className={style.tableBox}>
+                    <Col xs="2">문서번호</Col>
+                    <Col xs="3">기안일</Col>
+                    <Col xs="1">결제양식</Col>
+                    <Col xs="5">제목</Col>
+                    <Col xs="1">첨부</Col>
+                </Row>
+            </div>
+
         </Container>
     );
 }
