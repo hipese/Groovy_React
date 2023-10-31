3c74606cd3bc383c282e19dc1d1185d867bbcae3
Khwiba <typoon141@gmail.com>
2023-10-31 17:49:10 +0900

    2023-10-31-2

--- src/pages/Attendence/Attendence.js
+++ src/pages/Attendence/Attendence.js
@@ -2,8 +2,12 @@ import style from "./Attendence.module.css";
 
 const Attendence = () => {
     return (
-        <div className={style.myVacation}>내 연차 내역</div>
-        <div>연락</div>
+        <div className={style.Attendence}>
+            <div className={style.myVacation}>
+                내 연차 내역<button className={style.applyButton}>연차 신청하기</button>
+                <div>김민아 과장    총연차</div>
+            </div>
+        </div>
     );
 }
 
