4da29923b073b6e9c64f3ba7e039939c1d99ea05
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-10-31 17:38:42 +0900

    dashboard grid

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,7 +1,35 @@
+import style from './DashBoard.module.css';
+const Worksection = () => {
+    return(
+        <div className={style.worksection}>
+            asd
+        </div>
+    )
+}
+
+const Signsection = () => {
+    return (
+        <div className={style.signsection}>
+            sign
+        </div>
+    )
+}
+
+const Calandarsection = () => {
+    return (
+        <div className={style.calandarsection}>
+            calandar
+        </div>
+    )
+}
+
 const DashBoard=()=>{
     return(
-        <div>
-            dashboard
+        <div className={style.innerpad}>
+            <Worksection></Worksection>
+            <Signsection></Signsection>
+            <Calandarsection></Calandarsection>
+
         </div>
     )
 }
