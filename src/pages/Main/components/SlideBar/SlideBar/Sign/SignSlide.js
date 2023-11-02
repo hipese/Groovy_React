6e3a370750db06bfab67c49b9dfa647875fa4dcd
hipes <ansj2916@gmail.com>
2023-11-02 17:32:22 +0900

    작업도중 저장

--- src/pages/Main/components/SlideBar/SlideBar/Sign/SignSlide.js
+++ src/pages/Main/components/SlideBar/SlideBar/Sign/SignSlide.js
@@ -1,36 +1,39 @@
-import style from "./SignSlide.module.css";
-
+import style from "./SlideBar";
+import { Link } from "react-router-dom"
 
 const SignSlide = () => {
     return (
         <div className={style.SignSlide}>
 
             <div className={style.btndiv}>
-                <button className={style.writeBtn}><strong>전자 결재 작성</strong></button>
+                <Link to="Sign_Wirte">
+                    <button className={style.writeBtn}><strong>전자 결재 작성</strong></button>
+                </Link>
             </div>
 
 
             <div className={style.btndiv}>
+                <Link to="Sign_Main"></Link>
                 <button className={style.btn}>전자결제 홈</button>
             </div>
 
             <div className={style.btndiv}>
+                <Link to="Sign_Wirte"></Link>
                 <button className={style.btn}>새결제 진행</button>
             </div>
 
             <div className={style.btndiv}>
+                <Link to="Sign_Wait"></Link>
                 <button className={style.btn}>결제대기중</button>
             </div>
 
             <div className={style.btndiv}>
-                <button className={style.btn}>진행대기중</button>
-            </div>
-
-            <div className={style.btndiv}>
+                <Link to="Sign_Progress"></Link>
                 <button className={style.btn}>진행중문서</button>
             </div>
 
             <div className={style.btndiv}>
+                <Link to="Sign_Complete"></Link>
                 <button className={style.btn}>완료문서</button>
             </div>
 
