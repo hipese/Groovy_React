9756b85c46e356850bbdadf6398a6539de57edfd
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-11-02 17:32:06 +0900

    GROOV-43 Dashboard grid added

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,8 +1,18 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import style from './DashBoard.module.css';
+import { Grid } from '@mui/material';
+
+
 const Worksection = () => {
     return(
         <div className={style.worksection}>
-            asd
+            <p>근무체크</p>
+            <div className={style.contentsDiv}>
+                a
+            </div>  
         </div>
     )
 }
@@ -10,7 +20,10 @@ const Worksection = () => {
 const Signsection = () => {
     return (
         <div className={style.signsection}>
-            sign
+            <p>전자결재</p>
+            <div className={style.contentsDiv}>
+                a
+            </div>
         </div>
     )
 }
@@ -18,19 +31,35 @@ const Signsection = () => {
 const Calandarsection = () => {
     return (
         <div className={style.calandarsection}>
-            calandar
+            <p>달력</p>
+            
+        </div>
+    )
+}
+
+const ProjectSection = () => {
+    return (
+        <div className={style.projectsection}>
+            <p>프로젝트</p>
         </div>
     )
 }
 
 const DashBoard=()=>{
     return(
-        <div className={style.innerpad}>
-            <Worksection></Worksection>
-            <Signsection></Signsection>
-            <Calandarsection></Calandarsection>
-
+        <div className={style.guideDiv}>
+            <div className={style.left}>
+                <Worksection/>
+                <Signsection/>
+                <Calandarsection/>
+            </div>
+            <div className={style.right}>
+                <ProjectSection/>
+                <ProjectSection/>
+            </div>
         </div>
+        
+        
     )
 }
 
