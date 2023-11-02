8a4c4dbc29ec660d08cdd3ff3581502badaadb89
Khwiba <typoon141@gmail.com>
2023-11-02 16:47:59 +0900

    GROOV-52 vacationStatus add

--- src/pages/Attendence/Attendence.js
+++ src/pages/Attendence/Attendence.js
@@ -3,10 +3,27 @@ import style from "./Attendence.module.css";
 const Attendence = () => {
     return (
         <div className={style.Attendence}>
+            <div className={style.title}>내 연차 내역<button className={style.applyButton}>연차 신청하기</button></div><br />
             <div className={style.myVacation}>
-                내 연차 내역<button className={style.applyButton}>연차 신청하기</button>
-                <div>김민아 과장    총연차</div>
-            </div>
+                <div className={style.vacationStatus}>
+                    <div className={style.name}>
+                        <div>김민아 과장</div>
+                        <div>마케팅팀</div>
+                    </div>
+                    <div className={style.all}>
+                        <div>총연차</div>
+                        <div>15일</div>
+                    </div>
+                    <div className={style.use}>
+                        <div>사용연차</div>
+                        <div>9.5일</div>
+                    </div>
+                    <div className={style.left}>
+                        <div>잔여연차</div>
+                        <div>5.5일</div>
+                    </div>
+                </div>
+            </div>  
         </div>
     );
 }
