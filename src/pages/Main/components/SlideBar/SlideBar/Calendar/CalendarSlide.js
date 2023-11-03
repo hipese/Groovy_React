1f7807956a4174b09c5e8dcee360bbfb836d113c
kojaehoon <kojae0103@naver.com>
2023-11-03 09:02:42 +0900

    GROOV-37 TODOLIST SLIDEBAR MODIFIED

--- src/pages/Main/components/SlideBar/SlideBar/Calendar/CalendarSlide.js
+++ src/pages/Main/components/SlideBar/SlideBar/Calendar/CalendarSlide.js
@@ -1,31 +1,22 @@
     import styled from "styled-components";
     import { Routes, Route, Link } from "react-router-dom";
-    import style from "./CalendarSlide.module.css";
+    import style from "../SlideBar.module.css";
+    import styles from "./CalendarSlide.module.css";
 
     const CalendarWrite = () => {
         return (
-            <div className={style.CalendarSlide}>
-                <Link to="">
-                    <button className={style.btn}>
-                        <strong>+</strong> 캘린더
-                    </button>
-                </Link>
-                <Link to="Calendar2">
-                    <button className={style.btn}>
-                        <strong>+</strong> 일정
-                    </button>
-                </Link>
-                <Link to="Calendar3">
-                    <button className={style.btn}>
-                        <strong>+</strong> 일정2
-                    </button>
-                </Link>
-                <Link to="Calendar4">
-                    <button className={style.btn}>
-                        <strong>+</strong> 일정3
-                    </button>
-                </Link>
+            <div>
+                <button className={style.btn}>
+                    <strong>+</strong> 일정 추가
+                </button>
+                <div className={styles.Calendar}>
+                     캘린더 들어갈 자리
+                </div>
+                <div className={styles.MyCalendar}>
+                    내 캘린더
+                </div>
             </div>
+            
         );
     };
 
--- src/pages/Main/components/SlideBar/SlideBar/ToDoList/ToDoListSlide.js
+++ src/pages/Main/components/SlideBar/SlideBar/ToDoList/ToDoListSlide.js
@@ -1,31 +1,61 @@
-import style from "../SlideBar.module.css";
+import styles from "./ToDoListSlide.module.css";
 import { Link, Route, Routes } from "react-router-dom";
+import calendar from "./assets/calendar.png";
+import grid from "./assets/grid.png";
 
 const ToDoListSlide = () => {
     return (
-        <div className={style.ToDoListSlide}>
+        <div>
+            <div className={styles.workspace}>
+                {`OOO의 Workspace`}
+            </div>
+            <div className={styles.selectTitle}>
+                Workspace Views
+            </div>
             <Link to="">
-                <button className={style.btn}>
-                    <strong>+</strong> 일정
-                </button>
-            </Link>
-            <Link to="ToDoListBoard">
-                <button className={style.btn}>
-                    <strong>+</strong> 내 보드
-                </button>
+                <div className={styles.select}>
+                    <img src={grid} alt="where..?" width={"20px"} height={"20px"} /> <span className={styles.selectMenu}>Boards</span>
+                </div>
             </Link>
             <Link to="ToDoListCalendar">
-                <button className={style.btn}>
-                    <strong>+</strong> 캘린더
-                </button>
+                <div className={styles.select}>
+                    <img src={calendar} alt="where..?" width={"20px"} height={"20px"} /> <span className={styles.selectMenu}>Calendar</span>
+                </div>
             </Link>
+            <div className={styles.selectTitle}>
+                Team Boards <span className={styles.selectDown}><button className={styles.btn}>+</button></span>
+            </div>
             <Link to="ToDoListTeam">
-                <button className={style.btn}>
-                    <strong>+</strong> 팀 보드
-                </button>
+                <div className={styles.select}>
+                    <div className={styles.colorbox}></div> <span className={styles.selectMenu}>Team Board</span>
+                </div>
+            </Link>
+            <div className={styles.selectTitle}>
+                Your Boards <span className={styles.selectDown}><button className={styles.btn}>+</button></span>
+            </div>
+            <Link to="ToDoListBoard">
+                <div className={styles.select}>
+                    <div className={styles.colorbox}></div> <span className={styles.selectMenu}>My Board</span>
+                </div>
             </Link>
         </div>
     );
 };
 
 export default ToDoListSlide;
+
+            // <Link to="ToDoListBoard">
+            //     <button className={style.btn}>
+            //         <strong>+</strong> 내 보드
+            //     </button>
+            // </Link>
+            // <Link to="ToDoListCalendar">
+            //     <button className={style.btn}>
+            //         <strong>+</strong> 캘린더
+            //     </button>
+            // </Link>
+            // <Link to="ToDoListTeam">
+            //     <button className={style.btn}>
+            //         <strong>+</strong> 팀 보드
+            //     </button>
+            // </Link>
