1a2f58148b0a0c4690a8a203e8c7895c828d299b
hipes <ansj2916@gmail.com>
2023-11-07 13:37:33 +0900

    이미지 저장하기

--- src/pages/Mypage/components/Main_Mypage/MemberInfo/ImageChange/BtnDiv/BtnDiv.js
+++ src/pages/Mypage/components/Main_Mypage/MemberInfo/ImageChange/BtnDiv/BtnDiv.js
@@ -3,19 +3,29 @@ import axios from "axios";
 import style from "./BtnDiv.module.css"
 
 
-const BtnDiv = ({fileName,onClose}) => {
-    
-    console.log(fileName);
-    const handleImageChange=()=>{
-        axios.get(`/api/member/${fileName}`).then(resp=>{
-            console.log(resp);
-        })
+const BtnDiv = ({ cfile, onClose }) => {
+
+    console.log(cfile);
+    const handleImageChange = () => {
+
+        const formData = new FormData();
+        formData.append("cfile", cfile);
+
+        axios.post("/api/member/", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(resp => {
+            console.log(resp.data);
+        }).catch(error => {
+            console.error(error);
+        });
     }
 
     return (
         <div className={style.container}>
             <button onClick={handleImageChange} className={style.btn}>확인</button>
-            <button onClick={() => {onClose()}}  className={style.btn} >취소</button>
+            <button onClick={() => { onClose() }} className={style.btn} >취소</button>
         </div>
     );
 }
