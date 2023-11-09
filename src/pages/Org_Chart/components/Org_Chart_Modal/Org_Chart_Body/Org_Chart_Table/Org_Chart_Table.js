5bac6640c7831bf58db5c1018b4f4cfa970a5d8e
hipes <ansj2916@gmail.com>
2023-11-09 10:30:15 +0900

    GROOV-61 조직도 검색 기능 완료

--- src/pages/Org_Chart/components/Org_Chart_Modal/Org_Chart_Body/Org_Chart_Table/Org_Chart_Table.js
+++ src/pages/Org_Chart/components/Org_Chart_Modal/Org_Chart_Body/Org_Chart_Table/Org_Chart_Table.js
@@ -3,10 +3,10 @@ import style from "./Org_Chart_Table.module.css"
 import React, { useEffect, useState } from 'react';
 
 
-const Org_Chart_Table = ({ setEmployees,employees,selectedRow ,setSelectedRow,setBackUpEmployees}) => {
+const Org_Chart_Table = ({ setEmployees, employees, selectedRow, setSelectedRow, backUpEmployees }) => {
 
 
-    const [searchText,setSearchText] =useState("");
+    const [searchText, setSearchText] = useState("");
 
     // 행을 클릭했을 때 호출되는 함수입니다.
     const handleRowClick = (id) => {
@@ -20,13 +20,29 @@ const Org_Chart_Table = ({ setEmployees,employees,selectedRow ,setSelectedRow,se
         }
     };
 
-    const handleChange=(e)=>{
+    const handleChange = (e) => {
         setSearchText(e.target.value);
     }
 
-    const haadlesearch=()=>{
-        axios.get(`/api/member/${searchText}`).then(resp => {
+    const handleKeyDown = (e) => {
+        // 'Enter' 키가 눌렸는지 확인합니다.
+        if (e.key === 'Enter') {
+            haadlesearch();
+        }
+    };
+
+    const haadlesearch = (e) => {
+        console.log(searchText);
+        setEmployees(backUpEmployees);
+
+        if (searchText == null || searchText == "") {
+            alert("검색할 문자를 입력해주세요");
+            return;
+        }
+
+        axios.get(`/api/member/search/${searchText}`).then(resp => {
             console.log(resp.data);
+            setEmployees(resp.data);
         });
     }
 
@@ -34,7 +50,9 @@ const Org_Chart_Table = ({ setEmployees,employees,selectedRow ,setSelectedRow,se
     return (
         <div className={style.table}>
             <div className={style.table_header}>
-                <input className={style.table_input} type="text" name="searchText" value={searchText} placeholder="이름, 부서 검색" onChange={handleChange} />
+                <input className={style.table_input} type="text" name="searchText" value={searchText} 
+                placeholder="이름, 부서 검색" onChange={handleChange} onKeyDown={handleKeyDown} />
+
                 <button className={style.btn} onClick={haadlesearch} > 검색</button>
             </div>
 
@@ -48,7 +66,7 @@ const Org_Chart_Table = ({ setEmployees,employees,selectedRow ,setSelectedRow,se
 
 
                 <div className={style.contentsdiv}>
-                    {employees.map((employee, index) => (
+                    {employees && employees.length > 0 ? employees.map((employee, index) => (
                         <div
                             key={index}
                             className={`${style.table_row} ${selectedRow === employee.id ? style.highlight : ''}`}
@@ -59,7 +77,7 @@ const Org_Chart_Table = ({ setEmployees,employees,selectedRow ,setSelectedRow,se
                             <div className={style.table_col}>{employee.group_name}</div>
                             <div className={style.table_col}>{employee.id}</div>
                         </div>
-                    ))}
+                    )) : <div className={style.nullDiv}>검색결과가 존재하지 않습니다.</div>}
 
                 </div>
 
