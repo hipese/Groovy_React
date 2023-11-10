85498803b377fb1105ba6d5c5ac0863153cf5aa4
hipes <ansj2916@gmail.com>
2023-11-10 10:42:27 +0900

    GROOV-66 email 변경기능 추가

--- src/pages/Mypage/components/Main_Mypage/MemberInfo/Update/UpdateEmail.js
+++ src/pages/Mypage/components/Main_Mypage/MemberInfo/Update/UpdateEmail.js
@@ -9,14 +9,24 @@ const UpdateEmail=({onClose})=>{
     const members=useContext(MemberContext);
 
     const [email,setEmail]=useState(members.member.email||"");
-    
+    const [localPart, setLocalPart] = useState(members.member.email.split('@')[0] || ""); //@groovy제거 버전
 
     const handleChangeEmail = (e) => {
         const { name, value } = e.target;
-        setEmail(value);
+        setEmail(`${value}@groovy.com`);
+        setLocalPart(value)
     }
 
     const handleConfirm=()=>{
+
+        if (!localPart.trim()) { 
+            alert('Email의 앞부분을 입력해주세요. (@groovy.com은 자동으로 추가됩니다)'); // 경고창 띄우기
+            return; 
+        }else if (localPart.includes('@')) {
+            alert('Email 앞부분에 @ 기호를 포함할 수 없습니다. (@groovy.com은 자동으로 추가됩니다)');
+            return; // 함수를 여기서 종료
+        }
+        
         axios.put(`/api/member/emailUpdate/${email}`).then(resp => {
             console.log("email변경성공")
             onClose();
@@ -39,10 +49,10 @@ const UpdateEmail=({onClose})=>{
                 변경 후 Email
             </div>
             <div className={style.inputDiv}>
-                <input className={style.inputs} type="text" name="email" value={email} placeholder="Email을 입력하세요." onChange={handleChangeEmail}/>
+                <input className={style.inputs} type="text" name="email" value={localPart} placeholder="Email을 입력하세요.(@groovy는 빼고 입력)" onChange={handleChangeEmail}/>
             </div>
             <div className={style.contentsDiv}>
-                {email}
+                {`${localPart}@groovy.com`}
             </div>
             <div className={style.btnDiv}>
                 <button className={style.btn} onClick={handleConfirm}>확인</button>
