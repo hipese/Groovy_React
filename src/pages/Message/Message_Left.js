99b659274c83e5d00964d22a8705ca70e5a332a2
KMS <msg_1218@naver.com>
2023-11-14 13:27:21 +0900

    GROOV-30 채팅방 내부 UI 설계 및 기능 추가, websocket X

---
+++ src/pages/Message/Message_Left.js
@@ -0,0 +1,46 @@
+import { Col, Row } from "reactstrap";
+import style from "./Message_Chat_Component.module.css";
+import { useContext } from "react";
+import { ProfileContext, SelectContext } from "./Message";
+
+const MessageLeft = (props) => {
+    const { profiles } = useContext(ProfileContext);
+    const {selectedRoom} = useContext(SelectContext)
+    return(
+        <Col xs={12} className={style.msg_left_container}>
+            <Row className={style.msg_left}>
+                <Col xs={3} className={style.msg_left_img_container}>
+                    {
+                        profiles.filter(profile => profile.room_seq == selectedRoom).filter((profile) => profile.id === props.msg.id).map((profile, index) => {
+                            return (
+                                <div className={style.profile_box} key={index}>
+                                    {
+                                         profile.profile_image ?
+                                         <img src={`/profiles/${profile.profile_image}`} key={index} alt="profile" className={style.profile_image} ></img> :
+                                         <img src={`/assets/Default_pfp.svg`} key={index} alt="profile" className={style.profile_image}></img>
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </Col>
+                <Col xs={9} className={style.msg_left_info_container}>
+                    <Row className={style.msg_left_name_container}>
+                        <Col xs={12} className={style.msg_left_name}>
+                        {
+                            profiles.filter(profile => profile.id == props.msg.id)[0].name
+                        }
+                        </Col>
+                    </Row>
+                    <Row className={style.msg_left_contents_container}>
+                        <Col className={style.msg_left_contents}>
+                            {props.msg.contents}
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Col>
+    )
+}
+
+export default MessageLeft;
